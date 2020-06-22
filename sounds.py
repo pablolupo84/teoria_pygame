2589e7fa26337e7811e3eb7df58fed0a7408c50f
pablolupo84 <pablolupo84gmail.com>
2020-06-22 16:57:19 -0300

    Agregando sonido

--- sounds.py
+++ sounds.py
@@ -7,12 +7,20 @@ width=400
 height=600
 
 surface = pygame.display.set_mode((width,height)) # surface
-pygame.display.set_caption('Tiempo')
+pygame.display.set_caption('Sounds')
 
 #RGB
 red=pygame.Color(255,0,0) #0-255 
 white=(255,255,255)
 
+pygame.mixer.music.load('sounds/Haggstrom.mp3')
+pygame.mixer.music.set_volume(1.0) #Float 0.0-1.0
+pygame.mixer.music.play(-1,0.0)
+
+# pygame.mixer.music.rewind()
+# pygame.mixer.music.pause()
+# pygame.mixer.music.stop()
+# pygame.mixer.music.fadeout(5000)
 
 while True:
     for event in pygame.event.get():
@@ -20,11 +28,9 @@ while True:
             pygame.quit()
             sys.exit()
 
-    time=pygame.time.get_ticks() //1000 #milisengudos desde que se inicio la app
-    print(time)
     surface.fill(white)
-
-
+    
+    
     pygame.display.update()
 
 
