a3f2de52d3ab4c521674b755e87ad4dcf546385a
pablolupo84 <pablolupo84gmail.com>
2020-06-23 00:28:45 -0300

    Tecla presionada

---
+++ pressed.py
@@ -0,0 +1,39 @@
+import sys
+import pygame
+
+pygame.init()
+
+width=400
+height=600
+
+surface = pygame.display.set_mode((width,height)) # surface
+pygame.display.set_caption('Key Pressed')
+
+#RGB
+white=(255,255,255)
+
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pressed=pygame.key.get_pressed()
+    if pressed[pygame.K_w]:
+        print('Arriba')
+    if pressed[pygame.K_s]:
+        print('Abajo')
+    if pressed[pygame.K_a]:
+        print('Izquierda')
+    if pressed[pygame.K_d]:
+        print('Derecha')
+
+    surface.fill(white)
+    
+    
+    pygame.display.update()
+
+
+
